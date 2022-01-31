c67d63fa9083b02252032bc7b98a30df4da7792e
gabicantarini <gabilana64@hotmail.com>
2022-01-31 12:32:50 +0000

    feat: add check-email to test

--- main.py
+++ main.py
@@ -19,16 +19,23 @@ def create_client(request_body = Body(...)):
     name = request_body["name"]
     email = request_body["email"]
 
+    pattern = r'^[a-z 0-9]+[\._]?[a-z 0-9]+[@]\w+[.]\w{2,3}$'
+    #email = "gabrielacantarini@hotmail.com"
+
+    if re.find(pattern, email):
+        print("valid email")
+    else:
+        print("invalid email")
+    
     client = Client(name, email) 
     client_list.append(client)
-
     print(client_list)
 
     return client
 
 def check_email(email):
     pattern = r'^[a-z 0-9]+[\._]?[a-z 0-9]+[@]\w+[.]\w{2,3}$'
-    # email = "gabrielacantarini@hotmail.com"
+    #email = "gabrielacantarini@hotmail.com"
 
     if re.find(pattern, email):
         print("valid email")
